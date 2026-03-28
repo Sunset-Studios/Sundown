7266210f3e0fad900fccc4bf6923a106ed7abb9e
Adrian Sanchez <aj.adrian7@gmail.com>
2026-03-27 22:36:19 -0700

    Fix some issues with PTGI and mesh flicker when adding many entities

--- engine/src/renderer/render_graph.js
+++ engine/src/renderer/render_graph.js
@@ -1225,7 +1225,6 @@ export class RenderGraph {
    */
   submit() {
     profile_scope("RenderGraph.submit", () => {
-      this._reset_pass_cache_bind_groups();
       this._add_queued_post_commands();
       this._compile();
 
@@ -1254,10 +1253,9 @@ export class RenderGraph {
         GPUTimeQuery.resolve(encoder);
       }
 
-      CommandQueue.submit(encoder, this._execute_post_render_callbacks);
+      this._reset_pass_cache_bind_groups();
 
-      this.pass_cache_passes_needs_reset = false;
-      this.pass_cache_full_needs_reset = false;
+      CommandQueue.submit(encoder, this._execute_post_render_callbacks);
     });
   }
 
@@ -1953,8 +1951,10 @@ export class RenderGraph {
       this.pass_cache.bind_groups.keys().forEach((key) => {
         this.pass_cache.bind_groups.delete(key);
       });
+      this.pass_cache_passes_needs_reset = false;
     } else if (this.pass_cache_full_needs_reset) {
       this.pass_cache.bind_groups = new Map();
+      this.pass_cache_full_needs_reset = false;
     }
   }
 
