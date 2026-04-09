7c555a2140d6917c0740baf718811cee3026b191
Adrian Sanchez <aj.adrian7@gmail.com>
2026-04-08 17:31:22 -0700

    Fix include error in UI3DProcessor

--- engine/src/core/subsystems/ui_3d_processor.js
+++ engine/src/core/subsystems/ui_3d_processor.js
@@ -1,3 +1,4 @@
+import { EntityFlags } from "../minimal.js";
 import { SimulationLayer } from "../simulation_layer.js";
 import { EntityManager } from "../ecs/entity.js";
 import { UserInterfaceFragment } from "../ecs/fragments/user_interface_fragment.js";
